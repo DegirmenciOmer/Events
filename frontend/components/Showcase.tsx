f6db33e336a56bce08dbfafdb9504b894f88d50b
DegirmenciOmer <rumidegirmenci0@gmail.com>
2022-12-08 11:01:01 +0100

    initial fetch

--- frontend/components/Showcase.tsx
+++ frontend/components/Showcase.tsx
@@ -1,4 +1,4 @@
-import styles from "../styles/Showcase.module.css";
+import styles from "@/styles/Showcase.module.css";
 import React from "react";
 
 const Showcase = () => {
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,15 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import styles from "../styles/Home.module.css";
+import { API_URL } from "@/config/index";
 
 import React from "react";
 
-const Home = () => {
+const Home = ({ events }) => {
+  console.log(events);
+
   return (
     <Layout title="About Events">
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return { props: { events } };
+}
 
 export default Home;

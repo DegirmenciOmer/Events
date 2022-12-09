1be2dd7cf5bd74e33ae72882c8c4a4ed7af90ccc
DegirmenciOmer <rumidegirmenci0@gmail.com>
2022-12-09 20:39:48 +0100

    add EventItem, add static pages

--- frontend/pages/events/[slug].tsx
+++ frontend/pages/events/[slug].tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
 const EventPage = () => {
   return <Layout>EventPage</Layout>;
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events/`);
+  const events = await res.json();
+  // console.log(events[0]);
+
+  const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ query }) {
+  console.log({ query });
+
+  const res = await fetch(`${API_URL}/api/events/burgett`);
+  const events = await res.json();
+  return { props: { evt: events[0] }, revalidate: 1 };
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,25 @@
-import Head from "next/head";
-import Image from "next/image";
 import Layout from "../components/Layout";
+import EventItem from "../components/EventItem";
 import styles from "../styles/Home.module.css";
 import { API_URL } from "@/config/index";
 
 import React from "react";
+import Link from "next/link";
 
 const Home = ({ events }) => {
-  console.log(events);
-
   return (
     <Layout title="About Events">
       <h1>Upcoming Events</h1>
+      {events.length === 0 ? (
+        <h3>No events found</h3>
+      ) : (
+        events.map((evt) => <EventItem evt={evt} key={evt.id} />)
+      )}
+      {events.length > 0 && (
+        <Link href="/events">
+          <span className="btn-secondary"> View All Events</span>
+        </Link>
+      )}
     </Layout>
   );
 };

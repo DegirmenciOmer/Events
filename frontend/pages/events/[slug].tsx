90373f9bf403e7132dfc8d2958c2a7a0b425dc7f
DegirmenciOmer <rumidegirmenci0@gmail.com>
2022-12-09 22:45:35 +0100

    add EventPage structure

--- frontend/pages/events/[slug].tsx
+++ frontend/pages/events/[slug].tsx
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { FC } from "react";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Image from "next/image";
 
-const EventPage = (props) => {
-  console.log(props);
+interface TEvtProps {
+  evt: TEvt;
+}
 
-  return <Layout>EventPage</Layout>;
+export type TEvt = {
+  id: number;
+  address: string;
+  name: string;
+  slug: string;
+  date: string;
+  time: string;
+  venue: string;
+  description: string;
+  image: string;
+  performers: string;
+};
+
+const EventPage: FC<TEvtProps> = ({ evt }) => {
+  const deleteEvent = (e: any) => {
+    console.log("delete");
+  };
+  return (
+    <Layout>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id.toString()}`}>
+            <span>
+              <FaPencilAlt /> Edit Event
+            </span>
+          </Link>
+          <div className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </div>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={styles.image}>
+            <Image src={evt.image} width={960} height={600} />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go Back</a>
+        </Link>
+      </div>
+    </Layout>
+  );
 };
 
 export default EventPage;
@@ -13,7 +68,6 @@ export default EventPage;
 export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/api/events?slug=${slug}`);
   const events = await res.json();
-  console.log({ url: events });
 
   return {
     props: {
@@ -28,10 +82,9 @@ export async function getStaticPaths() {
 
   const events = await res.json();
 
-  const paths = events.map((evt) => ({
+  const paths = events.map((evt: TEvt) => ({
     params: { slug: evt.slug.toString() },
   }));
-  console.log(paths);
 
   return {
     paths,

6d0a1592bc34f5c15c1eafc4f610e14eb2a42215
DegirmenciOmer <rumidegirmenci0@gmail.com>
2022-12-26 18:07:21 +0100

    add concurrently, custom user events endpoint, get user events for dashboard

--- frontend/pages/account/dashboard.tsx
+++ frontend/pages/account/dashboard.tsx
@@ -1,8 +1,18 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import { parseCookies } from "helpers";
 import React from "react";
 
 const DashboardPage = () => {
-  return <Layout title="User dashboard">DashboardPage</Layout>;
+  return <Layout title="User Dashboard">DashboardPage</Layout>;
 };
-
+export const getServerSideProps = async ({ req }) => {
+  const { token } = parseCookies(req);
+  const res = await fetch(`${API_URL}/api/events/me`, {
+    method: "GET",
+  });
+  return {
+    props: {},
+  };
+};
 export default DashboardPage;

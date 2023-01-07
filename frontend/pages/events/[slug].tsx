b81df81c753978963088c43cef989afda54b6217
DegirmenciOmer <rumidegirmenci0@gmail.com>
2023-01-07 16:56:38 +0100

    fix test errors in slug page

--- frontend/pages/events/[slug].tsx
+++ frontend/pages/events/[slug].tsx
@@ -1,10 +1,11 @@
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import Layout from "@/components/Layout";
 import { API_URL } from "@/config/index";
 import styles from "@/styles/Event.module.css";
 import Link from "next/link";
 import Image from "next/image";
 import { ToastContainer } from "react-toastify";
+import { useRouter } from "next/router";
 
 interface TEvtProps {
   evt: TEvtData;
@@ -16,7 +17,6 @@ export type TEvtData = {
 };
 
 export type Tevt = {
-  id?: number;
   address: string;
   name: string;
   slug: string;
@@ -30,8 +30,16 @@ export type Tevt = {
 
 export type TEvents = { events: TEvtData[] };
 
-const EventPage: FC<TEvtProps> = ({ evt: { id, attributes: evt } }) => {
-  const date = new Date(evt.date).toLocaleDateString("nl-NL");
+const EventPage: FC<TEvtProps> = ({ evt }) => {
+  const router = useRouter();
+
+  const date = new Date(evt?.attributes?.date).toLocaleDateString("nl-NL");
+  useEffect(() => {
+    if (!evt) {
+      router.push("/404");
+      return;
+    }
+  }, [evt]);
 
   return (
     <Layout>
@@ -40,18 +48,19 @@ const EventPage: FC<TEvtProps> = ({ evt: { id, attributes: evt } }) => {
           <a className={styles.back}>{"<"} Go Back</a>
         </Link>
         <div className={styles.heading}>
-          <h1>{evt?.name}</h1>
+          <h1>{evt?.attributes?.name}</h1>
           <p>
-            {date} at {evt?.time}
+            {date} at {evt?.attributes?.time}
           </p>
         </div>
         <ToastContainer />
-        {evt?.image && (
+        {evt?.attributes?.image && (
           <div className={styles.image}>
             <Image
               src={
-                evt?.image?.data?.attributes?.formats?.medium ??
-                evt?.image?.data?.attributes?.formats?.thumbnail.url
+                evt?.attributes?.image?.data?.attributes?.formats?.medium ??
+                evt?.attributes?.image?.data?.attributes?.formats?.thumbnail
+                  ?.url
               }
               width={960}
               height={600}
@@ -59,12 +68,12 @@ const EventPage: FC<TEvtProps> = ({ evt: { id, attributes: evt } }) => {
           </div>
         )}
         <h3>Performers:</h3>
-        <p>{evt?.performers}</p>
+        <p>{evt?.attributes?.performers}</p>
         <h3>Description:</h3>
-        <p>{evt?.description}</p>
+        <p>{evt?.attributes?.description}</p>
         <h3>Venue: </h3>
         <p>
-          {evt?.address}, {evt?.venue}
+          {evt?.attributes?.address}, {evt?.attributes?.venue}
         </p>
       </div>
     </Layout>
@@ -73,31 +82,46 @@ const EventPage: FC<TEvtProps> = ({ evt: { id, attributes: evt } }) => {
 
 export default EventPage;
 
-export async function getStaticProps({ params: { slug } }) {
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(
+//     `${API_URL}/api/events?filters[slug][$eq]=${slug}&populate=*`
+//   );
+//   const { data: evt } = await res.json();
+
+//   return {
+//     props: {
+//       evt: evt[0],
+//     },
+//     revalidate: 1,
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/api/events/`);
+
+//   const { data: events } = await res.json();
+
+//   const paths = events.map((evt: TEvtData) => ({
+//     params: { slug: evt.attributes.slug },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(
     `${API_URL}/api/events?filters[slug][$eq]=${slug}&populate=*`
   );
+  console.log({ res });
+
   const { data: evt } = await res.json();
+  console.log({ evt });
 
   return {
     props: {
-      evt: evt[0],
+      evt: evt.length > 0 ? evt[0] : null,
     },
-    revalidate: 1,
-  };
-}
-
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/api/events/`);
-
-  const { data: events } = await res.json();
-
-  const paths = events.map((evt: TEvtData) => ({
-    params: { slug: evt.attributes.slug },
-  }));
-
-  return {
-    paths,
-    fallback: false,
   };
 }
